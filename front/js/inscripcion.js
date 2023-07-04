8968d027414a7c6b8836d45c9b72ec962edb87c3
Martin H <mlmh3113@hotmail.com>
2023-07-04 12:19:01 -0300

    correccion git

--- front/js/inscripcion.js
+++ front/js/inscripcion.js
@@ -10,19 +10,37 @@ createApp({
     };
   },
 
-    methods: {
-        fetchApi(){
-            fetch(this.url)
-            .then(res => res.jason())
-            .then(data =>{
-                this.inscripcion = data;
-                this.cargando = false;
-            })
-        .catch(err=>{
-            console.error(err);
-            this.error = true;
-            })
-        }
+  methods: {
+    fetchApi(url) {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          this.usuarios = data;
+          this.cargando = false;
+          const tabla = document.getElementById("tabla");
+          tabla.classList.remove("hide");
+        })
+        .catch((err) => {
+          console.error(err);
+          this.error = true;
+        });
+    },
+
+    getByDni(dni) {},
+
+    getByType(type) {},
+
+    delUser(dni) {
+      const url = this.url + "/" + dni;
+      let options = {
+        method: "DELETE",
+      };
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((data) => {
+          this.fetchApi(url);
+        })
+        .catch((err) => console.error(err));
     },
 
     editUser(user) {},
